6dcedf9bb79c4a1dffc058c6aa7c1e2b05cf4dd8
Billy <billyjag1@gmail.com>
2023-04-13 12:06:27 -0400

    avance1

---
+++ javascript/javascript.js
@@ -0,0 +1,35 @@
+
+
+function sumar() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    let resultado = (num1 + num2);
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+function restar() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    let resultado = (num1 - num2);
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+function multiplicar() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    let resultado = (num1 * num2);
+    document.getElementById('resultado').innerHTML = resultado;
+}
+
+function dividir() {
+    var num2 = parseInt(document.getElementById("num2").value);
+if(num2 == 0){
+    alert("No es posible divdir por 0");
+}else{
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    let resultado = (num1 / num2);
+    document.getElementById('resultado').innerHTML = resultado;
+}
+    
+}
